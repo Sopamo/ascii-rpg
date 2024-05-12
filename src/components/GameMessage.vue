f21d74b28185f44b5df54dd04b3c763c3d3ba0fb
Paul Mohr <p.mohr@sopamo.de>
2024-05-12 14:06:32 +0300

    avatars

--- src/components/GameMessage.vue
+++ src/components/GameMessage.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="system-message">
-    <div v-if="promptStore.currentMessage" class="message">
+  <div v-if="promptStore.currentMessage" class="system-message">
+    <img class="s-avatar" :src="promptStore.talkingTo ? `/img/${promptStore.talkingTo}.webp` : '/img/lake.webp'" />
+    <div class="message">
       <div>{{ promptStore.currentMessage.response }}</div>
       <div v-if="promptStore.currentMessage.inventoryActions?.add" class="s-itemsAdded">
         <div v-for="item in promptStore.currentMessage.inventoryActions.add" :key="item" class="s-itemsAdded__item">
@@ -8,31 +9,36 @@
         </div>
       </div>
       <div v-if="promptStore.currentMessage.inventoryActions?.remove" class="s-itemsRemoved">
-        <div v-for="item in promptStore.currentMessage.inventoryActions.remove" :key="item" class="s-itemsRemoved__item">
+        <div v-for="item in promptStore.currentMessage.inventoryActions.remove" :key="item"
+             class="s-itemsRemoved__item">
           - {{ item }}
         </div>
       </div>
-      <div v-if="promptStore.currentMessage.hpChange" class="s-hpChange" :class="{'-positive': promptStore.currentMessage.hpChange > 0}">
-        <template v-if="promptStore.currentMessage.hpChange > 0">+</template>{{ promptStore.currentMessage.hpChange }} HP
+      <div v-if="promptStore.currentMessage.hpChange" class="s-hpChange"
+           :class="{'-positive': promptStore.currentMessage.hpChange > 0}">
+        <template v-if="promptStore.currentMessage.hpChange > 0">+</template>
+        {{ promptStore.currentMessage.hpChange }} HP
       </div>
     </div>
+    <img v-if="usePlayerStore().characterId" class="s-avatar" :src="`/img/${usePlayerStore().characterId}.webp`" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { usePromptStore } from '@/stores/promptStore'
-import {  watch } from 'vue'
+import { watch } from 'vue'
+import { usePlayerStore } from '@/stores/playerStore'
 
 const promptStore = usePromptStore()
 
 watch(() => promptStore.messageHistory, () => {
-  const relevantEntries = promptStore.messageHistory.filter(entry => entry.role === "system")
-  if(relevantEntries.length === 0) {
+  const relevantEntries = promptStore.messageHistory.filter(entry => entry.role === 'system')
+  if (relevantEntries.length === 0) {
     return
   }
-  promptStore.currentMessage = relevantEntries[relevantEntries.length-1].content
+  promptStore.currentMessage = relevantEntries[relevantEntries.length - 1].content
 }, {
-  deep: true,
+  deep: true
 })
 </script>
 
@@ -40,9 +46,21 @@ watch(() => promptStore.messageHistory, () => {
 .system-message {
   position: relative;
   min-height: 45px;
+  display: flex;
+  flex-direction: row;
+  gap: 4px;
+}
+
+.s-avatar {
+  width: 70px;
+  height: 70px;
+  object-fit: cover;
+  border-radius: 2px;
+  border: 1px solid #dedede;
 }
 
 .message {
+  flex: 1;
   background-color: #000;
   border-radius: 3px;
   color: #fff;
@@ -58,14 +76,17 @@ watch(() => promptStore.messageHistory, () => {
   flex-direction: row;
   gap: 8px;
 }
+
 .s-itemsAdded__item {
   font-weight: bold;
   color: green;
 }
+
 .s-itemsRemoved__item {
   font-weight: bold;
   color: red;
 }
+
 .s-hpChange {
   font-weight: bold;
   color: red;
